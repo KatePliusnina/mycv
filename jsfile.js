f4e5fea114fc1842d97c4450562bce60e329fba8
KatePliusnina <92167821+KatePliusnina@users.noreply.github.com>
2021-11-17 01:18:45 +0200

    Add files via upload

---
+++ jsfile.js
@@ -0,0 +1,11 @@
+function ButtonAdress(){
+    alert("Kyiv, Zadorozhny lane 6-А, apt.607");
+}  
+
+function hide(){
+    document.getElementById('hider').onclick=function(){
+       document.getElementById('text').hidden= true;
+    }
+}
+
+
